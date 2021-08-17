ef9ba22af080ac57ccd801103c364e22cb4bb76b
ingakonovalova <ingakonovalova@gmail.com>
2021-08-17 13:50:06 +0300

    Adding weather adjectives from csv

--- messageGenerator.js
+++ messageGenerator.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
-let cities = ["Tartu", "Tallinn", "Pärnu", "Viljandi"];
-let weather = ["windy", "foggy", "rainy", "sunny", "cloudy", "nice", "wet", "humid", "stormy", "lovely", "gloomy"];
+let cities = [];
+let weather = [];
 let temperature = Math.floor(Math.random() * (30 - 0) + 0);
 let coldOrWarm = ["above", "below"];
 
@@ -16,7 +16,7 @@ function readData(file) {
     var data = '';
     const re = /,\r\n/;
 
-    data = fs.readFileSync('cities.csv', 'utf8' , (err, data) => {
+    data = fs.readFileSync(file, 'utf8' , (err, data) => {
         if (err) {
           console.error(err)
         }
@@ -27,4 +27,6 @@ function readData(file) {
 }
 
 cities = readData("cities.csv");
+weather = readData("weather.csv");
+
 console.log("The weather in " + takeRandom(cities) + " today is " + takeRandom(weather) +  " and the temperature outside is " + temperature + " degrees " + takeRandom(coldOrWarm) + " zero.");
